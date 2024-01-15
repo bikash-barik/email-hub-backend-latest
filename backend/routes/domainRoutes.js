1cf81af7f43d3e16908475c0051cf026741018ed
sarthak Singh <100351826+sarthaksingh007@users.noreply.github.com>
2024-01-15 19:29:32 +0530

    initial commit

---
+++ backend/routes/domainRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+// import { protect } from "../../middleware/authMiddleware.js";
+// import {uploadFile} from "../../controllers//domainController.js";
+import {domainCountry} from "../controllers/domainController.js";
+import {extractEmail} from "../controllers/domainController.js";
+import {validateEmail} from "../controllers/domainController.js";
+import { uploadFile } from "../controllers/domainController.js";
+
+const router = express.Router();
+
+router.route("/").post(uploadFile);
+router.route("/domainCountry").post(domainCountry);
+router.route("/extractEmail").post(extractEmail);
+router.route("/validateEmail").post(validateEmail);
+
+export default router;
